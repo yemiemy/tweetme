39e08cee66ced4334c0ca3a4511c07ec232f9143
Yemiemy <rasholayemi@gmail.com>
2021-10-28 08:57:05 +0100

    Users and Tweets

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.conf import settings
 from random import randint
 import humanize
+
+User = settings.AUTH_USER_MODEL
 # Create your models here.
 
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True, max_length=240)
     image = models.ImageField(upload_to='tweets/images', blank=True, null=True)
     date_stamp = models.DateTimeField(auto_now_add=True)
