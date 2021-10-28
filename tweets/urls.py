e5e0d1d87a667bccb139f13ec13adc29e1c77c79
Yemiemy <rasholayemi@gmail.com>
2021-10-28 03:23:49 +0100

    Implemented Bootstarp for templates

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import home, tweet_list_view, tweet_detail_view
 urlpatterns = [
-    path('', home),
-    path('tweets', tweet_list_view),
+    path('', home, name='home'),
+    path('tweets', tweet_list_view, name="tweets"),
     path('tweets/<int:tweet_id>/', tweet_detail_view)
 ]
