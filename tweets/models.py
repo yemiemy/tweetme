655fa0fd011a49b2bea7edc5a75b525443a4d381
Yemiemy <rasholayemi@gmail.com>
2021-10-29 14:47:02 +0100

    Adding a Like field

--- tweets/models.py
+++ tweets/models.py
@@ -6,14 +6,20 @@ import humanize
 User = settings.AUTH_USER_MODEL
 # Create your models here.
 
+class TweetLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.TimeField(auto_now_add=True)
+
 class Tweet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True, max_length=240)
     image = models.ImageField(upload_to='tweets/images', blank=True, null=True)
-    date_stamp = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField( , related_name="tweet_user", blank=True, through=TweetLike)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 
-    class Meta:
+    class Meta: 
         ordering = ['-id']
 
     def serialize(self):
@@ -21,5 +27,5 @@ class Tweet(models.Model):
             "id":self.id, 
             "content":self.content, 
             "likes":randint(1,100), 
-            "date_stamp":humanize.naturalday(self.date_stamp)
+            "timestamp":humanize.naturalday(self.timestamp)
         }
