4b4511c48762cc4d66ad9c38f28247d9724c9b99
Yemiemy <rasholayemi@gmail.com>
2021-10-30 00:54:48 +0100

    Understanding Functional Components

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,13 +1,26 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [tweets, setTweets] = useState([{content: 123}])
+
+  useEffect(() => {
+    // do my lookup
+    const tweetItems = [{"content": 123}, {"content": "Test 1"}, {"content": "Hello World!"}]
+    setTweets(tweetItems)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {tweets.map((tweet, index)=>{
+            return <li>{tweet.content}</li>
+          })}
         </p>
         <a
           className="App-link"
