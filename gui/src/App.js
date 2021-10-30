84f9c42499ca074145085227c698c1473e9dd301
Yemiemy <rasholayemi@gmail.com>
2021-10-30 01:19:18 +0100

    Ajax lookup via XHR in ReactJs

--- gui/src/App.js
+++ gui/src/App.js
@@ -2,13 +2,37 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function loadTweets(callback){
+  const xhr = new XMLHttpRequest()
+  const method ="GET"
+  const url = "http://127.0.0.1:8000/api/tweets/"
+  const responseType = "json"
+
+  xhr.responseType = responseType
+  xhr.open(method, url)
+  xhr.onload = function(){
+    callback(xhr.response, xhr.status)
+  }
+  xhr.onerror =  function (e) {
+    console.log(e)
+    callback({"message":"The request was an error."}, 400)
+  }
+  xhr.send()
+}
+
+
 function App() {
   const [tweets, setTweets] = useState([{content: 123}])
 
   useEffect(() => {
     // do my lookup
-    const tweetItems = [{"content": 123}, {"content": "Test 1"}, {"content": "Hello World!"}]
-    setTweets(tweetItems)
+    const myCallback = (response, status) => {
+      console.log(response, status)
+      if (status === 200){
+        setTweets(response)
+      }
+    }
+    loadTweets(myCallback)
   }, [])
   return (
     <div className="App">
