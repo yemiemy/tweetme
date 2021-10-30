24c41ee23e1b20eb26a10229ff5f336cfab76902
Yemiemy <rasholayemi@gmail.com>
2021-10-30 02:51:59 +0100

    Functional Components in React

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,63 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-      const cookies = document.cookie.split(';');
-      for (let i = 0; i < cookies.length; i++) {
-          const cookie = cookies[i].trim();
-          // Does this cookie string begin with the name we want?
-          if (cookie.substring(0, name.length + 1) === (name + '=')) {
-              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-              break;
-          }
-      }
-  }
-  return cookieValue;
-}
-
-function handleTweetAction(tweet_id, currentCount, action) {
-  const url = "{% url 'tweet_action' %}"
-  const method = "POST"
-  const data = JSON.stringify({
-      id: tweet_id,
-      action: action
-  })
-  const xhr = new XMLHttpRequest()
-  const csrftoken = getCookie('csrftoken');
-  xhr.open(method, url)
-  xhr.setRequestHeader("Content-Type", "application/json")
-  xhr.setRequestHeader("HTTP_X_REQUESTED_WITH", "XMLHttpRequest")
-  xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest")
-  xhr.setRequestHeader("X-CSRFToken", csrftoken)
-  xhr.onload = function () {
-      // loadTweets(tweetsContainerElement)
-  }
-  xhr.send(data)
-}
-
-function LikeBtn(tweet){
-  return `
-  <a onclick='handleTweetAction(${tweet.id}, ${tweet.likes}, "like")'>
-      <i class="fe fe-heart mr-1 text-primary"></i><small>${tweet.likes}</small>
-  </a>`
-}
-
-function UnlikeBtn(tweet){
-  return `
-  <a onclick='handleTweetAction(${tweet.id}, ${tweet.likes}, "unlike")'>
-      <i class="fe fe-thumbs-down mr-1 text-danger"></i><small>${tweet.likes}</small>
-  </a>`
-}
-
-function RetweetBtn(tweet){
-  return `
-  <a onclick='handleTweetAction(${tweet.id}, ${tweet.likes}, "retweet")'>
-      <i class="fe fe-refresh-ccw mr-1 text-primary"></i><small>${tweet.likes}</small>
-  </a>`
-}
+import Tweet from './containers/TweetFormat';
 
 
 function loadTweets(callback){
@@ -78,6 +20,8 @@ function loadTweets(callback){
   xhr.send()
 }
 
+
+
 function App() {
   const [tweets, setTweets] = useState([{content: 123}])
 
@@ -92,7 +36,7 @@ function App() {
     loadTweets(myCallback)
   }, [])
   return (
-    <div className="App">
+    <div>
       <div className="py-lg-6 py-4 bg-primary">
         <div className="container">
           <div className="row">
@@ -105,7 +49,7 @@ function App() {
         </div>
       </div>
 
-    <div className="py-6">
+      <div className="py-6">
         <div className="container"> 
             <div className="card">
                 <div className="card-body">
@@ -128,62 +72,15 @@ function App() {
                     </div>
                     <div className="dropdown-divider"></div>
                     <div id="tweets">
-                      {tweets.map((tweet, index)=>{
-                        return `
-                          <div class="d-lg-flex align-items-center justify-content-between">
-                          <div class="d-flex align-items-center mb-4 mb-lg-0">
-                            <img src="{% static 'assets/images/avatar/avatar.jpg' %}" id="img-uploaded" class="avatar-lg rounded-circle" alt="" />
-                            <div class="ml-3">
-                              <h4 class="mb-0">Nickname <small style="color:grey">@username ${tweet.timestamp}</small></h4>
-                              <p class="mb-0" id='tweet-${tweet.id}'>
-                                  ${tweet.content}
-                              </p>
-                              <div class='btn-group d-flex' style="cursor:pointer;">
-                                  <div>
-                                      ${LikeBtn(tweet)}
-                                  </div>
-                                  <div class="ml-3">
-                                      ${UnlikeBtn(tweet)}
-                                  </div>
-                                  <div class="ml-3">
-                                      ${RetweetBtn(tweet)}
-                                  </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="dropdown-divider"></div>
-                        `
+                      {tweets.map((item, index)=>{
+                        return <Tweet tweet={item} key={`${index}-{item.id}`}/>
                       })}
                       
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-
-
-
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          {tweets.map((tweet, index)=>{
-            return <li>{tweet.content}</li>
-          })}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
---
+++ gui/src/containers/TweetFormat.js
@@ -0,0 +1,103 @@
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+  }
+  
+  
+
+function handleTweetAction(tweet_id, currentCount, action) {
+    const url = "{% url 'tweet_action' %}"
+    const method = "POST"
+    const data = JSON.stringify({
+        id: tweet_id,
+        action: action
+    })
+    const xhr = new XMLHttpRequest()
+    const csrftoken = getCookie('csrftoken');
+    xhr.open(method, url)
+    xhr.setRequestHeader("Content-Type", "application/json")
+    xhr.setRequestHeader("HTTP_X_REQUESTED_WITH", "XMLHttpRequest")
+    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest")
+    xhr.setRequestHeader("X-CSRFToken", csrftoken)
+    xhr.onload = function () {
+        // loadTweets(tweetsContainerElement)
+    }
+    xhr.send(data)
+  }
+  
+function LikeBtn(props){
+    const {tweet} = props
+    return (
+    // eslint-disable-next-line jsx-a11y/anchor-is-valid
+    <a onclick='handleTweetAction({tweet.id}, {tweet.likes}, "like")'>
+        <i className="fe fe-heart mr-1 text-primary"></i><small>{tweet.likes}</small>
+    </a>
+    )
+  }
+  
+  function UnlikeBtn(props){
+    const {tweet} = props
+    return (
+      // eslint-disable-next-line jsx-a11y/anchor-is-valid
+      <a onclick='handleTweetAction({tweet.id}, {tweet.likes}, "unlike")'>
+        <i className="fe fe-thumbs-down mr-1 text-danger"></i><small>{tweet.likes}</small>
+    </a>
+    )
+    
+  }
+  
+  function RetweetBtn(props) {
+    const {tweet} = props
+    return (
+      // eslint-disable-next-line jsx-a11y/anchor-is-valid
+      <a onclick='handleTweetAction({tweet.id}, {tweet.likes}, "retweet")'>
+        <i className="fe fe-refresh-ccw mr-1 text-primary"></i><small>{tweet.likes}</small>
+      </a>
+    )
+  }
+  
+  
+
+function Tweet(props) {
+    const {tweet} = props
+    return (
+      <div>
+        <div className="d-lg-flex align-items-center justify-content-between">
+          <div className="d-flex align-items-center mb-4 mb-lg-0">
+            <img src="%PUBLIC_URL%/assets/images/avatar/avatar.jpg' %}" id="img-uploaded" className="avatar-lg rounded-circle" alt="" />
+            <div className="ml-3">
+              <h4 className="mb-0">Nickname <small Style="color:grey">@username {tweet.timestamp}</small></h4>
+              <p className="mb-0" id='tweet-{tweet.id}'>
+                  {tweet.content}
+              </p>
+              <div className='btn-group d-flex' Style="cursor:pointer;">
+                  <div>
+                      {/* <LikeBtn props={tweet} /> */}
+                  </div>
+                  <div className="ml-3">
+                      {/* <UnlikeBtn props={tweet} /> */}
+                  </div>
+                  <div className="ml-3">
+                      {/* <RetweetBtn props={tweet} /> */}
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="dropdown-divider"></div>
+      </div>
+    )
+  }
+
+  export default Tweet;
