ce5fef449fee128cdf1da0e4dd18448de8c7f2e5
Yemiemy <rasholayemi@gmail.com>
2021-11-01 22:40:25 +0100

    Dev Authentication

--- tweetme/settings.py
+++ tweetme/settings.py
@@ -142,11 +142,17 @@ DEFAULT_RENDERER_CLASSES = [
         'rest_framework.renderers.JSONRenderer',
     ]
 
+DEFAULT_AUTHENTICATION_CLASSES = [
+    'rest_framework.authentication.SessionAuthentication'
+    ]
+
 if DEBUG:
     DEFAULT_RENDERER_CLASSES += ['rest_framework.renderers.BrowsableAPIRenderer']
+    DEFAULT_AUTHENTICATION_CLASSES += ['tweetme.rest_api.dev.DevAuthentication']
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES
+    'DEFAULT_RENDERER_CLASSES': DEFAULT_RENDERER_CLASSES,
+    'DEFAULT_AUTHENTICATION_CLASSES':DEFAULT_AUTHENTICATION_CLASSES
 }
 
 # Default primary key field type
