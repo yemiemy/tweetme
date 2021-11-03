d85785851361bbeb2e113a591affff052184e024
Yemiemy <rasholayemi@gmail.com>
2021-11-03 01:42:46 +0100

    Rendering the Retweet

--- gui/src/tweets/components.js
+++ gui/src/tweets/components.js
@@ -18,9 +18,7 @@ function ActionBtn(props){
     
     const handleClick = (event) => {
       event.preventDefault()
-      if (action.type === 'like'){
-        apiTweetAction(tweet.id, action.type, handleActionBackendEvent)     
-      }
+      apiTweetAction(tweet.id, action.type, handleActionBackendEvent) 
     }
     return action.type === "like" ? (
       // eslint-disable-next-line jsx-a11y/anchor-is-valid
@@ -36,26 +34,41 @@ function ActionBtn(props){
   }
 
 
+export function ParentTweet(props){
+  const {tweet} = props
+  return tweet.parent ? <div className="border">
+                          <small className="ml-3">Retweeted</small>
+                          <Tweet className={'d-none'} tweet={tweet.parent} />
+                        </div>
+    : null
+} 
+
 function Tweet(props) {
     const {tweet} = props
+
+    const className = props.className ? props.className : 'avatar-lg rounded-circle'
+
     return (
       <div>
         <div className="d-lg-flex align-items-center justify-content-between">
           <div className="d-flex align-items-center mb-4 mb-lg-0">
-            <img src="/avatar.jpg" id="img-uploaded" className="avatar-lg rounded-circle" alt="" />
+            <img src="/avatar.jpg" id="img-uploaded" className={className} alt="" />
             <div className="ml-3">
-              <h4 className="mb-0">Nickname <small style={{color:"grey"}}>@username {tweet.timestamp}</small></h4>
-              <p className="mb-0" id='tweet-{tweet.id}'>
-                  {tweet.content}
-              </p>
-              <div className='btn-group d-flex' style={{cursor:"pointer"}}>
-                  <div>
-                    <ActionBtn tweet={tweet} action={{type: "like", display:"fe fe-heart mr-1 text-primary"}} />
-                  </div>
-                  <div className="ml-3">
-                    <ActionBtn tweet={tweet} action={{type: "retweet", display:"fe fe-refresh-ccw mr-1 text-primary"}} />
-                  </div>
+              <div>
+                <h4 className="mb-0">Nickname <small style={{color:"grey"}}>@username {tweet.timestamp}</small></h4>
+                <p className="mb-0" id='tweet-{tweet.id}'>
+                    {tweet.content}
+                </p>
+                <div className='btn-group d-flex' style={{cursor:"pointer"}}>
+                    <div>
+                      <ActionBtn tweet={tweet} action={{type: "like", display:"fe fe-heart mr-1 text-primary"}} />
+                    </div>
+                    <div className="ml-3">
+                      <ActionBtn tweet={tweet} action={{type: "retweet", display:"fe fe-refresh-ccw mr-1 text-primary"}} />
+                    </div>
+                </div>
               </div>
+              <ParentTweet tweet = {tweet}/>
             </div>
           </div>
         </div>
